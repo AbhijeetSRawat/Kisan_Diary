9087491476758e908a52b1849f9d43695b85cbd6
Allokyadav05 <allokyadav05@gmail>
2025-02-18 23:23:23 +0530

    alok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 
 import { Route, Routes } from 'react-router-dom'
 import Gehun from './pages/gehun'
+import { Cheena } from './pages/motaanaz/Cheena'
+import { Gahat } from './pages/motaanaz/Gahat'
+import { Jhangora } from './pages/motaanaz/Jhangora'
+import { Kakun } from './pages/motaanaz/Kakun'
+import {Kutki} from './pages/motaanaz/Kutki'
+import { Ramdana } from './pages/motaanaz/Ramdana'
 
 
+// } from './pages/motaanaz/Kuttu'
 export default function App() {
   return (
     <div>
@@ -13,6 +20,7 @@ export default function App() {
         <Routes>
             <Route path='/' element={<Gehun/>}/>
             <Route path='*' element="page not found"/>
+           
         </Routes>
       </div>
     </div>
