8c91a2201a47263f6ed82d67720963ffeed7c400
Abhijeet Singh Rawat <rawatab2@gmail.com>
2025-02-19 11:36:04 +0530

    chabj

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import logo from './assets/imgages/logo1.jpg'
 import joinedImages from './assets/imgages/joinedimages.jpg'
 import { Route, Routes } from 'react-router-dom';
@@ -7,9 +7,9 @@ import Chapter1 from './pages/chapter1';
 import Exportfool from './pages/falfool/Exportfool';
 import KhadyanFasle from './components/KhadyanFasle';
 import Gehun from './pages/gehun';
-import Chana from './pages/Dal/chana';
+// import Chana from './pages/Dal/chana';
 import DalhaniFasle from './components/DalhaniFasle';
-import Matar from './pages/Dal/matar';
+// import Matar from './pages/Dal/matar';
 import Masoor from './pages/Dal/masoor';
 import Urd from './pages/Dal/urd';
 import Moong from './pages/Dal/moong';
@@ -111,8 +111,8 @@ const App = () => {
                 <Route path='/KhadyanFasle' element={<KhadyanFasle/>}/>
                 <Route path='/DalhaniFasle' element={<DalhaniFasle/>}/>
                 <Route path='/gehun' element={<Gehun/>}/>
-                <Route path='/chana' element={<Chana/>}/>
-                <Route path='/matar' element={<Matar/>}/>
+                {/* <Route path='/chana' element={<Chana/>}/> */}
+                {/* <Route path='/matar' element={<Matar/>}/> */}
                 <Route path='/masoor' element={<Masoor/>}/>
                 <Route path='/urad' element={<Urd/>}/>
                 <Route path='/moong' element={<Moong/>}/>
