c02d582440815a25cc5f1ca24ecb78517705b9a6
Abhijeet Singh Rawat <rawatab2@gmail.com>
2025-02-20 01:23:47 +0530

    dummy commit

--- src/components/Motaanaj.jsx
+++ src/components/Motaanaj.jsx
@@ -5,13 +5,13 @@ export default function Motaanaj() {
   return (
     <div>
         <ul className='flex flex-col gap-3'>
-        <li><Link className='text-2xl text-blue-900 ' to="/madua"> 1.मंडुवा झंगोरा (मादिरा/सांवा)</Link></li>
-        <li><Link className='text-2xl text-blue-900 ' to="/kakun"> 2.काकुन</Link></li>
-        <li><Link className='text-2xl text-blue-900 ' to="/cheena"> 3.चीना</Link></li>
-        <li><Link className='text-2xl text-blue-900 ' to="/ramdana"> 4.रामदाना</Link></li>
-        <li><Link className='text-2xl text-blue-900 ' to="/ricebean"> 5.राइसबीन (नौरंगी)</Link></li>
-        <li><Link className='text-2xl text-blue-900 ' to="/gahat"> 6.गहत (कुल्थी)</Link></li>
-        <li><Link className='text-2xl text-blue-900 ' to="/parichay"> 7.मोटे अनाज-परिचय, आहार में महत्व एवं पोषकता</Link></li>
+        <li><Link className='text-2xl text-blue-900 ' to="/madua"> 1. मंडुवा झंगोरा (मादिरा/सांवा)</Link></li>
+        <li><Link className='text-2xl text-blue-900 ' to="/kakun"> 2. काकुन</Link></li>
+        <li><Link className='text-2xl text-blue-900 ' to="/cheena"> 3. चीना</Link></li>
+        <li><Link className='text-2xl text-blue-900 ' to="/ramdana"> 4. रामदाना</Link></li>
+        <li><Link className='text-2xl text-blue-900 ' to="/ricebean"> 5. राइसबीन (नौरंगी)</Link></li>
+        <li><Link className='text-2xl text-blue-900 ' to="/gahat"> 6. गहत (कुल्थी)</Link></li>
+        <li><Link className='text-2xl text-blue-900 ' to="/parichay"> 7. मोटे अनाज-परिचय, आहार में महत्व एवं पोषकता</Link></li>
 
         </ul>
     </div>
