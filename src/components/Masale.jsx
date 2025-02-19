f6c391c14148f9c40cc0a44181a136b9343e6d63
Abhijeet Singh Rawat <rawatab2@gmail.com>
2025-02-19 22:40:42 +0530

    skjfsk

--- src/components/Masale.jsx
+++ src/components/Masale.jsx
@@ -7,7 +7,7 @@ export default function Masale() {
         <ul className='flex flex-col gap-3'>
         <li><Link className='text-2xl text-blue-900 ' to="/pyaj"> 1.प्याज</Link></li>
         <li><Link className='text-2xl text-blue-900 ' to="/dhaniya"> 2.धनिया</Link></li>
-        <li><Link className='text-2xl text-blue-900 ' to="/methi"> 3.मेंथी</Link></li>
+        <li><Link className='text-2xl text-blue-900 ' to="/methi"> 3.मेथी</Link></li>
         <li><Link className='text-2xl text-blue-900 ' to="/lehsun"> 4.लहसुन</Link></li>
         <li><Link className='text-2xl text-blue-900 ' to="/haldi"> 5.हल्दी</Link></li>
         <li><Link className='text-2xl text-blue-900 ' to="/adrak"> 6.अदरक</Link></li>
